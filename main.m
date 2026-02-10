4118bc760fa3939778704cfaf82eb9361de713cc
ma1266 <ma1266@nb-ma1266>
2026-02-10 17:13:23 +0100

    save data in main and read with readFile

--- main.m
+++ main.m
@@ -11,6 +11,7 @@ for i=1:100
     x(i) = -C1*(i/10)^2 + C2*(i/10)^3;
 end
 
+save("data.mat", "x")
 
 %% plot
 figure(1)
---
+++ readFile.m
@@ -0,0 +1,6 @@
+
+clear all
+load("data.mat")
+
+figure(1), hold on
+plot(x, 'b-.')
